32f917c179cf6eb75646318d2daa30d5a525f987
StoikoNeykov <stoikoneykov@gmail.com>
2016-04-24 18:31:09 +0300

    more practice

---
+++ CSharp1/C1Practice/AngryFemaleGPS/AngryFemaleGPS.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+
+class AngryFemaleGPS
+{
+    static void Main()
+    {
+        long num = long.Parse(Console.ReadLine());
+        if (num < 0)
+        {
+            num = -num;
+        }
+        long sumEven = 0;
+        long sumOdd = 0;
+        while (num != 0)
+        {
+            if (num % 2 == 0)
+            {
+                sumEven += num % 10;
+            }
+            else
+            {
+                sumOdd += num % 10;
+            }
+            num /= 10;
+        }
+        if (sumOdd == sumEven)
+        {
+            Console.WriteLine("straight {0}", sumEven);
+        }
+        else if (sumEven > sumOdd)
+        {
+            Console.WriteLine("right {0}", sumEven);
+        }
+        else
+        {
+            Console.WriteLine("left {0}", sumOdd);
+        }
+    }
+}
+
+
+
---
+++ CSharp1/C1Practice/PeaceOfCake/PeaceOfCake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class PeaceOfCake
+{
+    static void Main()
+    {
+        ulong a = ulong.Parse(Console.ReadLine());
+        ulong b = ulong.Parse(Console.ReadLine());
+        ulong c = ulong.Parse(Console.ReadLine());
+        ulong d = ulong.Parse(Console.ReadLine());
+        if ((a * d + b * c) / (b * d) >= 1)
+        {
+            Console.WriteLine((a * d + b * c) / (b * d));
+        }
+        else
+        {
+            decimal result = (decimal)(a * d + b * c) /(decimal)(b * d);
+            Console.WriteLine("{0:f22}", Math.Round(result, 22));
+        }
+        Console.WriteLine("{0}/{1}", a * d + b * c, b * d);
+    }
+}
+
