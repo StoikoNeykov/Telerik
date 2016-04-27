4bd0421a9398c0810e80ea0900826110c637e47e
StoikoNeykov <stoikoneykov@gmail.com>
2016-04-27 12:11:49 +0300

    Exam

---
+++ CSharp1/C1Practice/ConsoleApplication2/ConsoleApplication2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Numerics;
+
+class ConsoleApplication2
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        string input = Console.ReadLine();
+        string toTen = null;
+        BigInteger product = 1;
+        int counter = 0;
+        while (input != "END")
+        {
+            if ((counter % 2 == 0) && (input != "0"))
+            {
+                while (input != string.Empty)
+                {
+                    if ((input[input.Length - 1] != '0')&& (input[input.Length - 1] != '-'))
+                    {
+                        product *= (input[input.Length - 1] - '0');
+                    }
+                    input = input.Substring(0, input.Length - 1);
+                }
+
+            }
+            if (counter == 9)
+            {
+                toTen = product.ToString();
+                product = 1;
+            }
+            counter++;
+            input = Console.ReadLine();
+        }
+
+
+
+        //output
+        if (counter > 9)
+        {
+            Console.WriteLine(toTen);
+            Console.WriteLine(product);
+
+        }
+        else
+        {
+            Console.WriteLine(product);
+        }
+    }
+}
+
---
+++ CSharp1/examc1/Speeds/Speeds.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class Speeds
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        int cars = int.Parse(Console.ReadLine());
+        double maxLine = 0;
+        double maxSpeed = 0;
+        double curent = 0;
+        double curentLine = 0;
+        double curentSpeed = 0;
+        double exSpeed=0;
+        for (int i = 0; i < cars; i++)
+        {
+          
+            curent = double.Parse(Console.ReadLine());
+            if (i == 0)
+            {
+                exSpeed = curent;
+            }
+            if (curent>exSpeed)
+            {
+                curentSpeed += curent;
+                curentLine++;
+            }
+            else
+            {
+                exSpeed = curent;
+                curentLine = 1;
+                curentSpeed = curent;
+            }
+            if (curentLine>maxLine)
+            {
+                maxLine = curentLine;
+                maxSpeed = curentSpeed;
+            }
+            else if ((curentLine==maxLine)&&(curentSpeed>maxSpeed))
+            {
+                maxSpeed = curentSpeed;
+            }
+            
+        }
+        Console.WriteLine(maxSpeed);
+    }
+}
+
