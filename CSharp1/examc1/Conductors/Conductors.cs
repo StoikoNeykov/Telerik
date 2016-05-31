8a36b21530439a4cd068037c12a1d468a95b88c5
StoikoNeykov <stoikoneykov@gmail.com>
2016-05-31 06:40:32 +0300

    c1 exam

---
+++ CSharp1/examc1/Conductors/Conductors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Conductors
+{
+    static void Main()
+    {
+        int p = int.Parse(Console.ReadLine());
+        int nums = int.Parse(Console.ReadLine());
+        int patern = Convert.ToString(p, 2).Length;
+        int curent;
+        var output = new List<int>();
+        for (int i = 0; i < nums; i++)
+        {
+            curent = int.Parse(Console.ReadLine());
+            if (curent >= p)
+            {
+                for (int j = 0; j < 32 - patern; j++)
+                {
+                    int temp = curent >> j;
+                    temp = temp << 32 - patern;
+                    int mask = p << 32 - patern;
+                    if (mask == temp)
+                    {
+                        curent = curent ^ (p << j);
+                    }
+                }
+            }
+
+
+            output.Add(curent);
+        }
+        foreach (var item in output)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ CSharp1/examc1/SumOfEvenDivisors/SumOfEvenDivisors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class SumOfEvenDivisors
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        long sum = 0;
+        if (a > b) //make sure b is bigger
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        for (int i = a; i <= b; i++)
+        {
+            for (int j = 2; j <= i; j++)
+            {
+                if ((i % j == 0) && (j % 2 == 0))
+                {
+                    sum += j;
+                }
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
+
