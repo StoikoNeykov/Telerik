05c1fb1e49f0c3c436b2cf2c2472705b10d71cb5
StoikoNeykov <stoikoneykov@gmail.com>
2016-06-30 06:03:15 +0300

    replace tags 100/100

--- JavaScriptFundamentals/11. Strings/08. replace-tags.js
+++ JavaScriptFundamentals/11. Strings/08. replace-tags.js
@@ -2,28 +2,28 @@ function solve(args) {
     var StartsWith = /^<a href="/,
         regexBegin = /<a href="/g,
         matchExtractLable = /(.*?)">(.*?)<\/a>(.*)/,
-        result,
+        part,
         splited,
-        part = '';
+        result = '';
 
     splited = args[0].split(regexBegin);
 
     if (!args[0].match(StartsWith)) {
-        part += splited.shift();
+        result += splited.shift();
     }
 
-    result = splited.map(m => {
+    part = splited.map(m => {
         var match = m.match(matchExtractLable);
-        if(match) {
-        return '[' + match[2] + '](' + match[1] + ')' + match[3];
+        if (match) {
+            return '[' + match[2] + '](' + match[1] + ')' + match[3];
         } else {
             return m;
         }
     });
 
-    part += result.join('');
+    result += part.join('');
 
-    console.log(part);
+    console.log(result);
 }
 
 text = ['<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>'];
